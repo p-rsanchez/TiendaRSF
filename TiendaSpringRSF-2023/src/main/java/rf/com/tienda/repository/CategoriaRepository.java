78e42cf74e9bb913d44834742691ea4fa61a51af
Rafael Sánchez Fernández <129056124+p-rsanchez@users.noreply.github.com>
2023-04-19 13:30:22 +0200

    Delete TiendaSpringRSF-2023 directory

--- TiendaSpringRSF-2023/src/main/java/rf/com/tienda/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package rf.com.tienda.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import rf.com.tienda.dominio.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
-
-}
